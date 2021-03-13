0c0ce61514c16316e1c5064f29a1d8ba4e33a41f
Aman <aman.bitmesra@gmail.com>
2021-03-13 03:59:25 +0100

    Kiara

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Aman here.
+         Kiara here.
         </p>
         <a
           className="App-link"
